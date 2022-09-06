837aeafc6f7b0919e5b952c10541fd121b982523
Shin Chen <jiayuanchen@outlook.com>
2022-09-06 16:48:38 +1000

    Update startup.py
    fix pdset default

--- pythonstartup/startup.py
+++ pythonstartup/startup.py
@@ -632,40 +632,41 @@ def _set_digit(digit):
     pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 
-def pdset(opt = '', value = -1):
+def pdset(opt = 'default', value = -1):
     '''
     Quick way to set pandas option
     :param opt: pandas option
     :param value: optioin value
     :return: none
     '''
+    opt = opt.lower()
+
     # default setting
-    if opt == '' and value == -1:
+    if opt == 'default' and value == -1:
         pd.set_option('display.max_column', 15)
         pd.set_option('display.max_rows', 50)
         pd.set_option('display.max_colwidth', 100)
         pd.set_option('display.width', 300)
         pd.set_option('display.float_format', lambda x: '%.4f' % x)
-    
-    opt = opt.lower()
 
-    if opt == 'mc':
-        pd.set_option('display.max_column', value)
-        print (f'pd.set_option(display.max_column, {value})')
-    elif opt == 'mr':
-        pd.set_option('display.max_rows', value)
-        print (f'pd.set_option(display.max_rows, {value})')
-    elif opt == 'cw':
-        pd.set_option('display.max_colwidth', value)
-        print (f'pd.set_option(display.max_colwidth, {value})')
-    elif opt == 'w':
-        pd.set_option('display.width', value)
-        print (f'pd.set_option(display.width, {value})')
-    elif opt == 'd':
-        pd.set_option('display.float_format', lambda x: f'%.{value}f' % x)
-        print (f'pd.set_option(display.float_format, lambda x: %.{value}f % x)')
     else:
-        raise KeyError('No matching option, please check the option keywords')
+        if opt == 'mc':
+            pd.set_option('display.max_column', value)
+            print (f'pd.set_option(display.max_column, {value})')
+        elif opt == 'mr':
+            pd.set_option('display.max_rows', value)
+            print (f'pd.set_option(display.max_rows, {value})')
+        elif opt == 'cw':
+            pd.set_option('display.max_colwidth', value)
+            print (f'pd.set_option(display.max_colwidth, {value})')
+        elif opt == 'w':
+            pd.set_option('display.width', value)
+            print (f'pd.set_option(display.width, {value})')
+        elif opt == 'd':
+            pd.set_option('display.float_format', lambda x: f'%.{value}f' % x)
+            print (f'pd.set_option(display.float_format, lambda x: %.{value}f % x)')
+        else:
+            raise KeyError('No matching option, please check the option keywords')
 
 import inspect
 
